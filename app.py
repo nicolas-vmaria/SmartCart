5e8fdb2c0845311179332ecf2c9869fc85ff71c3
Caio Boing <boing.caio@gmail.com>
2025-11-16 10:16:46 -0300

    sqlalchemy e Flask-Admin incopleto

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-import os
-from flask import Flask, render_template, request, redirect
-from models import Usuario
+from flask import Flask, render_template, request, redirect, url_for
+from db import db
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from models import Usuario, Produto
 from flask_login import (
     LoginManager,
     login_required,
@@ -8,12 +10,24 @@ from flask_login import (
     logout_user,
     current_user,
 )
-import mysql.connector
 import hashlib
+
+admin = Admin()
+
+def init_app(app):
+    admin.name = "Loja de Informática Admin"
+    admin.template_mode = "bootstrap3"
+    admin.init_app(app)
 
 app = Flask(__name__)
 app.secret_key = "chaveteste"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+db.init_app(app)
+admin.init_app(app)
 lm = LoginManager(app)
+
+admin.add_view(ModelView(Produto, db.session))
+admin.add_view(ModelView(Usuario, db.session))
 
 
 def hash(txt):
@@ -22,26 +36,10 @@ def hash(txt):
 
 
 @lm.user_loader
-def load_user(id):
-    cursor.execute("SELECT * FROM Usuarios WHERE id=%s", (id,))
-    resultado = cursor.fetchone()
-    if resultado:
-        return Usuario(
-            resultado["id"],
-            resultado["nome"],
-            resultado["cpf"],
-            resultado["telefone"],
-            resultado["email"],
-            resultado["senha"],
-        )
-
-
-conexao = mysql.connector.connect(
-    host="localhost", port="3306", user="root", password="12345678", database="SmartCart"
-)
-cursor = conexao.cursor(dictionary=True)
-
-
+def user_loader(id):
+    usuario = db.session.query(Usuario).filter_by(id=id).first()
+    return usuario
+    
 @app.route("/")
 def index():
     return render_template("index.html", user=current_user)
@@ -56,76 +54,61 @@ def sobre():
 @login_required
 def logout():
     logout_user()
-    return redirect("/")
+    return redirect(url_for("index"))
 
 
 @app.route("/cadastro", methods=["GET", "POST"])
 def cadastro():
     if request.method == "GET":
-        return render_template("/cadastro.html")
+        return render_template("cadastro.html")
     elif request.method == "POST":
         nome = request.form["nome"]
         cpf = request.form["cpf"].replace(".", "").replace("-", "")
         telefone = request.form["tel"].replace("(", "").replace(")", "").replace("-", "").replace(" ", "")
         email = request.form["email"]
-        senha = request.form["senha"]
+        senha = request.form["senha"]  
 
-        cursor.execute("SELECT * FROM Usuarios WHERE cpf=%s OR email=%s", (cpf, email))
-        usuario_existente = cursor.fetchone()
+        usuario_existente = Usuario.query.filter( (Usuario.cpf == cpf) | (Usuario.email == email)).first()
 
         if usuario_existente:
             erro = "CPF ou E-mail já cadastrado"
             return redirect(f"/cadastro?erro={erro}")
         else:
-            cursor.execute(
-                "INSERT INTO Usuarios (nome, cpf, telefone, email, senha) VALUES (%s, %s, %s, %s, %s)",
-                (nome, cpf, telefone, email, hash(senha)),
-            )
-            conexao.commit()
+            new_user = Usuario(nome=nome, cpf=cpf, telefone=telefone, email=email, senha=hash(senha))
 
-            new_user = Usuario(cursor.lastrowid, nome, cpf, telefone, email, senha)
+            db.session.add(new_user)
+            db.session.commit()
             login_user(new_user)
 
-            return redirect("/")
+            return redirect(url_for("index"))
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
-        return render_template("/login.html")
+        return render_template("login.html")
     elif request.method == "POST":
         cpf = request.form["cpf"].replace("-", "").replace(".", "")
         senha = request.form["senha"]
 
-        cursor.execute(
-            "SELECT * FROM Usuarios WHERE cpf=%s AND senha=%s", (cpf, hash(senha)) 
-        )
-        user_old = cursor.fetchone()
+        usuario = Usuario.query.filter_by(cpf=cpf, senha=hash(senha)).first()
 
-        if user_old:
-            user = Usuario(
-                user_old["id"],
-                user_old["nome"],
-                user_old["cpf"],
-                user_old["telefone"],
-                user_old["email"],
-                user_old["senha"],
-            )
-            login_user(user)
-            return redirect("/")
+        if usuario:
+            login_user(usuario)
+            return redirect(url_for("index"))
         else:
-            erro = "Senha ou nome já cadastrado"
+            erro = "CPF ou senha incorretos"
             return redirect(f"/login?erro={erro}")
 
 
 @app.route("/produtos")
 def listar_produtos():
-    cursor.execute("SELECT * FROM Produtos ORDER BY nome ASC")
-    produtos = cursor.fetchall()
-    return render_template("produto.html", produtos=produtos, user=current_user)
-
+    produtos = db.session.query(Produto).all()
+    return render_template("produto.html", user=current_user, produtos=produtos)
 
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,10 +1,23 @@
+from db import db
 from flask_login import UserMixin
 
-class Usuario(UserMixin):
-    def __init__(self, id, nome, cpf, telefone, gmail, senha):
-        self.id = id
-        self.nome = nome
-        self.cpf = cpf
-        self.telefone = telefone
-        self.gmail = gmail
-        self.senha = senha
+
+class Usuario(db.Model, UserMixin):
+    __tablename__ = "usuarios"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    cpf = db.Column(db.String(11), unique=True, nullable=False)
+    telefone = db.Column(db.String(11), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    senha = db.Column(db.String(64), nullable=False)
+
+class Produto(db.Model):
+    __tablename__ = "produtos"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    preco = db.Column(db.Float, nullable=False)
+    estoque = db.Column(db.Integer, nullable=False)
+    id_imagem = db.Column(db.String(200), nullable=True)
+
