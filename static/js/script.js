3c4dab413b19da2c20760d8e7f2bd06498377e16
FELIPE BARBOSA <felipe_barbosa153@estudante.sesisenai.org.br>
2025-11-18 14:08:35 -0300

    update landing page

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    controller: {
+        inverse: true,
+    },
+    navigation: {
+        nextEl: ".button-right",
+        prevEl: ".button-left"
+    }
+
+});
