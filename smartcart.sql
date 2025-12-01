bc02a13e2bb4f0d908c85237bd0ea58890cd9687
Nicolas Vieira Maria <nicolasvmaria1@gmail.com>
2025-11-30 22:27:54 -0300

    footer e descriçao dos produtos

--- smartcart.sql
+++ smartcart.sql
@@ -16,7 +16,7 @@ CREATE TABLE Produtos (
     preco DECIMAL(10 , 2 ) NOT NULL,
     estoque INT DEFAULT 0,
     id_imagem VARCHAR(20),
-    descricao VARCHAR(200) NOT NULL
+    descricao VARCHAR(500) NOT NULL
 );
 
 CREATE TABLE Orcamentos (
@@ -67,12 +67,12 @@ CREATE TABLE Pedidos (
 );
 
 
-insert into Produtos (nome, preco, estoque, id_imagem) values
-("SmartCart", 649.00, 5000, "img1.png"),
-("DoubleCart", 760.00, 1000, "img2"),
-("SmartGlide", 490.00, 800, "img3"),
-("SmartConfort ", 399.00, 50, "img4"),
-("SmartDrop", 180.00, 3000, "img5");
+insert into Produtos (nome, preco, estoque, id_imagem, descricao) values
+("SmartCart", 649.00, 5000, "img1.png","O SmartCart é a evolução da sua experiência em loja. Projetado com design aerodinâmico e materiais de alta resistência, ele oferece uma navegação suave e silenciosa pelos corredores. Sua estrutura ergonômica reduz o esforço físico, enquanto o cesto inteligente maximiza o espaço sem comprometer a mobilidade. Ideal para quem busca durabilidade, estilo e eficiência em cada compra."),
+("DoubleCart", 760.00, 1000, "img2","Não deixe nada para trás com o DoubleCart. Desenvolvido para compras de grande volume ou abastecimento mensal, este modelo conta com dois níveis de armazenamento robustos. Apesar de sua capacidade superior, mantém a leveza no manuseio graças ao seu sistema de rodízios de alta performance. A solução perfeita para otimizar o transporte de múltiplos itens com total segurança."),
+("SmartGlide", 490.00, 800, "img3","O SmartGlide redefine a praticidade. Com seu sistema de 'puxe' inteligente, ele é perfeito para compras rápidas e corredores mais estreitos. Sua alça telescópica ajustável se adapta à sua altura, garantindo conforto total. Leve, compacto e extremamente manobrável, o SmartGlide é a escolha certa para quem valoriza agilidade sem abrir mão do espaço interno."),
+("SmartConfort ", 399.00, 50, "img4","Transforme as compras em família em um momento tranquilo com o SmartConfort. Equipado com uma cadeirinha de bebê integrada, segura e higiênica, este carrinho foi desenhado pensando na proteção dos pequenos e na conveniência dos pais. Estrutura estável e cesto acessível garantem que você possa focar no que precisa comprar, sabendo que seu filho está confortável e seguro."),
+("SmartDrop", 180.00, 3000, "img5","Para aquelas visitas rápidas à loja, a SmartDrop é imbatível. Feita de material ultrarresistente e leve, possui alças anatômicas que não machucam as mãos, mesmo com peso. Design moderno que facilita o empilhamento e a limpeza, sendo a companheira ideal para quem precisa de velocidade e praticidade no dia a dia.");
 
 
 ALTER TABLE Usuario ADD COLUMN is_admin BOOLEAN DEFAULT FALSE;
