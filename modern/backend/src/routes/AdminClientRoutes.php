829967efbe35671409f6fb33762ee063777b8d57
Nicolas Vieira Maria <nicolasvmaria1@gmail.com>
2026-05-18 15:23:14 -0300

    Backend Clients

--- modern/backend/src/routes/AdminClientRoutes.php
+++ modern/backend/src/routes/AdminClientRoutes.php
@@ -3,6 +3,4 @@
 require_once __DIR__ . '/../controller/AdminClientController.php';
 
 $router->get('/admin/client',          [AdminClientController::class, 'index']);
-$router->post('/admin/client',         [AdminClientController::class, 'store']);
-$router->put('/admin/client/{id}',     [AdminClientController::class, 'update']);
 $router->delete('/admin/client/{id}',  [AdminClientController::class, 'destroy']);
